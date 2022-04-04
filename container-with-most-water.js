0d0d4741e9b8266904b6554880168831da4e67de
Vien Huynh Van <hvvien@axonactive.com>
2022-04-04 12:54:09 +0700

    Container with most water

---
+++ container-with-most-water.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let max = 0;
+  let i = 0;
+  let j = height.length - 1;
+  while (i < j) {
+    let now = 0;
+    if (height[i] < height[j]) {
+      now = height[i] * (j - i);
+      i++;
+    } else {
+      now = height[j] * (j - i);
+      j--;
+    }
+    if (now > max) {
+      max = now;
+    }
+  }
+  return max;
+};
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+console.log(maxArea([1, 1]));
