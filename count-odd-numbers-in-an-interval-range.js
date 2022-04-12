bf02a4344c270e53fbbb1207d96db3358e13c3f4
vienhuynhemc <hvvien2k@gmail.com>
2022-04-12 15:12:52 +0700

    count odd numbers in an interval range

---
+++ count-odd-numbers-in-an-interval-range.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function (low, high) {
+  let count = 0;
+  for (let i = low; i <= high; i++) {
+    if (i % 2 == 1) {
+      i++;
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(countOdds(3, 7));
+console.log(countOdds(8, 10));
