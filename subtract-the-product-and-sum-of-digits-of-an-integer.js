913751d97e0820f91de514f0667f73ddc3d78dbc
vienhuynhemc <hvvien2k@gmail.com>
2022-04-15 16:15:31 +0700

    subtract the product and sum of digits of an integer

---
+++ subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function (n) {
+  let input = n + "";
+  let sum = 0;
+  let mul = 0;
+  for (let i = 0; i < input.length; i++) {
+    sum += parseInt(input[i]);
+    if (i == 0) {
+      mul = parseInt(input[i]);
+    } else {
+      mul *= parseInt(input[i]);
+    }
+  }
+  return mul - sum;
+};
+
+console.log(subtractProductAndSum(234));
+console.log(subtractProductAndSum(4421));
