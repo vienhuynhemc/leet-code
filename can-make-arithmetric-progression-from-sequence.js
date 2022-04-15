07ff30e6e1ae56813321e623ef488bb04a1e8ccd
vienhuynhemc <hvvien2k@gmail.com>
2022-04-15 16:40:54 +0700

    can make arithmetric progression from sequence

---
+++ can-make-arithmetric-progression-from-sequence.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canMakeArithmeticProgression = function (arr) {
+  arr.sort((a, b) => a - b);
+  let diff = arr[1] - arr[0];
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] != diff) return false;
+  }
+  return true;
+};
+
+console.log(canMakeArithmeticProgression([3, 5, 1]));
+console.log(canMakeArithmeticProgression([1, 2, 4]));
