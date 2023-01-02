da6bf08c880056a22af174e38fcde2593c793be9
Huỳnh Văn Viên <73946516+vienhuynhemc@users.noreply.github.com>
2023-01-02 17:22:44 +0700

    word pattern

---
+++ WordPattern.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordPattern {
+
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.trim().split(" ");
+        if (pattern.length() != words.length) return false;
+        Map<Character, String> charWithIndex = new HashMap<>();
+        for (int i = 0; i < pattern.length(); i++) {
+            char charAtI = pattern.charAt(i);
+            if (charWithIndex.containsKey(charAtI)) {
+                if (!charWithIndex.get(charAtI).equals(words[i])) return false;
+            } else {
+                if (charWithIndex.containsValue(words[i])) return false;
+                charWithIndex.put(charAtI, words[i]);
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        WordPattern wordPattern = new WordPattern();
+        System.out.println(wordPattern.wordPattern("abba", "dog cat cat dog"));
+        System.out.println(wordPattern.wordPattern("abba", "dog cat cat fish"));
+        System.out.println(wordPattern.wordPattern("aaaa", "dog cat cat dog"));
+    }
+
+}
