8a131f1886a25a15bb117382a1d7a6bcc7a3c02d
Huỳnh Văn Viên <73946516+vienhuynhemc@users.noreply.github.com>
2023-04-25 17:17:25 +0700

    find the index of the first occurrence in a string

---
+++ FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,13 @@
+public class FindTheIndexOfTheFirstOccurrenceInAString {
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+    public static void main(String[] args) {
+        FindTheIndexOfTheFirstOccurrenceInAString findTheIndexOfTheFirstOccurrenceInAString = new FindTheIndexOfTheFirstOccurrenceInAString();
+        System.out.println(findTheIndexOfTheFirstOccurrenceInAString.strStr("sadbutsad", "sad"));
+        System.out.println(findTheIndexOfTheFirstOccurrenceInAString.strStr("leetcode", "leeto"));
+    }
+
+}
