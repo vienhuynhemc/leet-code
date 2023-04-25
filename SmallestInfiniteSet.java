b7de98757e772d3bac3bdc356f3eb121b94afb67
Huỳnh Văn Viên <73946516+vienhuynhemc@users.noreply.github.com>
2023-04-25 17:13:58 +0700

    smallest infinite set

---
+++ SmallestInfiniteSet.java
@@ -0,0 +1,33 @@
+import java.util.Iterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+class SmallestInfiniteSet {
+
+    private int start;
+    private final SortedSet<Integer> set;
+
+    public SmallestInfiniteSet() {
+        this.set = new TreeSet<>();
+        this.start = 1;
+    }
+
+    public int popSmallest() {
+        int result = start;
+        Iterator<Integer> iterator = this.set.iterator();
+        if (iterator.hasNext()) {
+            result = iterator.next();
+            this.set.remove(result);
+        } else {
+            start++;
+        }
+        return result;
+    }
+
+    public void addBack(int num) {
+        if (num < start) {
+            this.set.add(num);
+        }
+    }
+
+}
