f33f337e5f0aa9e887a08b5651eed3705a3ade18
Huỳnh Văn Viên <73946516+vienhuynhemc@users.noreply.github.com>
2023-04-25 18:06:48 +0700

    jump game

---
+++ JumpGame.java
@@ -0,0 +1,52 @@
+import load.LoadArray;
+import load.LogTimeExecute;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class JumpGame {
+
+    private final Set<Integer> tracking;
+
+    public JumpGame() {
+        this.tracking = new HashSet<>();
+    }
+
+    public boolean canJump(int[] nums) {
+        if (nums.length == 0) {
+            return true;
+        }
+        this.tracking.clear();
+        return jump(0, nums);
+    }
+
+    private boolean jump(int index, int[] nums) {
+        if (index == nums.length - 1) {
+            return true;
+        }
+        if (this.tracking.contains(index)) {
+            return false;
+        }
+        if (index >= nums.length || nums[index] == 0) {
+            this.tracking.add(index);
+            return false;
+        }
+        for (int i = nums[index]; i > 0; i--) {
+            boolean canJump = jump(index + i, nums);
+            if (canJump) {
+                return true;
+            }
+        }
+        this.tracking.add(index);
+        return false;
+    }
+
+    public static void main(String[] args) {
+        JumpGame jumpGame = new JumpGame();
+        System.out.println(jumpGame.canJump(new int[]{2, 3, 1, 1, 4}));
+        System.out.println(jumpGame.canJump(new int[]{3, 2, 1, 0, 4}));
+        int[] largeArray = LoadArray.getInstance().loadIntArrayFromFile("data/jump-game.txt");
+        LogTimeExecute.getInstance().log(unused -> jumpGame.canJump(largeArray));
+    }
+
+}
