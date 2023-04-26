3b5bd94d278134c714516112f63c1cc2b6911db5
Huỳnh Văn Viên <73946516+vienhuynhemc@users.noreply.github.com>
2023-04-26 22:15:38 +0700

    find first and last position of element in sorted array

---
+++ FindFirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+public class FindFirstAndLastPositionOfElementInSortedArray {
+
+    public int[] searchRange(int[] nums, int target) {
+        int index = this.binarySearch(nums, 0, nums.length - 1, target);
+        int startPosition = index;
+        int endPosition = index;
+        if (startPosition != -1) {
+            while (startPosition > 0) {
+                if (nums[startPosition - 1] != target) {
+                    break;
+                }
+                startPosition--;
+            }
+            while (endPosition < nums.length - 1) {
+                if (nums[endPosition + 1] != target) {
+                    break;
+                }
+                endPosition++;
+            }
+        }
+        return new int[]{startPosition, endPosition};
+    }
+
+    private int binarySearch(int[] arr, int l, int r, int x) {
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == x) {
+                return mid;
+            }
+            if (arr[mid] > x) {
+                return binarySearch(arr, l, mid - 1, x);
+            }
+            return binarySearch(arr, mid + 1, r, x);
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        FindFirstAndLastPositionOfElementInSortedArray findFirstAndLastPositionOfElementInSortedArray = new FindFirstAndLastPositionOfElementInSortedArray();
+        System.out.println(Arrays.toString(findFirstAndLastPositionOfElementInSortedArray.searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));
+        System.out.println(Arrays.toString(findFirstAndLastPositionOfElementInSortedArray.searchRange(new int[]{5, 7, 7, 8, 8, 10}, 6)));
+        System.out.println(Arrays.toString(findFirstAndLastPositionOfElementInSortedArray.searchRange(new int[]{}, 0)));
+    }
+
+}
