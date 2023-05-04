10380e6a65c3273d107a59e254fe92727cabd493
Huỳnh Văn Viên <73946516+vienhuynhemc@users.noreply.github.com>
2023-05-04 20:05:39 +0700

    reverse linked list

---
+++ ReverseLinkedList.java
@@ -0,0 +1,26 @@
+import models.ListNode;
+
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        if (next == null) {
+            return head;
+        }
+        head.next = null;
+        return reverseList(next, head);
+    }
+
+    private ListNode reverseList(ListNode head, ListNode before) {
+        ListNode next = head.next;
+        head.next = before;
+        if (next == null) {
+            return head;
+        }
+        return reverseList(next, head);
+    }
+
+}
