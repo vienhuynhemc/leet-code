ba477bbc105cdf41b8316d83304eaaf83ee866e1
Huỳnh Văn Viên <73946516+vienhuynhemc@users.noreply.github.com>
2023-05-05 17:13:16 +0700

    load string

---
+++ load/LoadString.java
@@ -0,0 +1,29 @@
+package load;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LoadString {
+
+    private static LoadString loadString;
+
+    public static LoadString getInstance() {
+        if (loadString == null) {
+            loadString = new LoadString();
+        }
+        return loadString;
+    }
+
+    public String loadStringFromFile(String src) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(src));
+            String currentLine = bufferedReader.readLine();
+            bufferedReader.close();
+            return currentLine;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
