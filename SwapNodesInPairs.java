68139c07ba05aacc64f54290aa86aa08a5fcf4f2
Huỳnh Văn Viên <73946516+vienhuynhemc@users.noreply.github.com>
2023-05-06 13:00:47 +0700

    swap nodes in pairs

---
+++ SwapNodesInPairs.java
@@ -0,0 +1,28 @@
+import models.ListNode;
+
+public class SwapNodesInPairs {
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        swapPairs(head, head.next);
+        return head;
+    }
+
+    private void swapPairs(ListNode first, ListNode second) {
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+        if (second.next == null || second.next.next == null) {
+            return;
+        }
+        swapPairs(second.next, second.next.next);
+    }
+
+    public static void main(String[] args) {
+        SwapNodesInPairs swapNodesInPairs = new SwapNodesInPairs();
+        System.out.println(swapNodesInPairs.swapPairs(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))))));
+    }
+
+}
