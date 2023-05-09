710d95b64c769d91210665c8799a6f3f0d88ea5d
Huỳnh Văn Viên <73946516+vienhuynhemc@users.noreply.github.com>
2023-05-09 20:29:09 +0700

    binary tree level order traversal

---
+++ BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,29 @@
+import models.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeLevelOrderTraversal {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        levelOrder(root, 0, result);
+        return result;
+    }
+
+    private void levelOrder(TreeNode root, int level, List<List<Integer>> result) {
+        if (root == null) {
+            return;
+        }
+        if (result.size() == level) {
+            result.add(new ArrayList<>());
+        }
+        result.get(level).add(root.val);
+        levelOrder(root.left, level + 1, result);
+        levelOrder(root.right, level + 1, result);
+    }
+
+}
