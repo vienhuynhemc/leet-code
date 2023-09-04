abd0c8f2aefde56ffacd37355681040c9b6fb9f9
Huỳnh Văn Viên <vien.huynh@mesoneer.io>
2023-09-04 10:11:27 +0700

    Resolve 141. Linked List Cycle

---
+++ LinkedListCycle.java
@@ -0,0 +1,25 @@
+import models.ListNode;
+
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode fast = head.next;
+        ListNode slow = head;
+
+        while (fast != slow) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+    }
+
+}
