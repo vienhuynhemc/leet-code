ae909303e29de101a20fb1cefe21d7d57d0de407
Huỳnh Văn Viên <vien.huynh@mesoneer.io>
2024-03-08 14:33:48 +0700

    Resolve 3005. Count Elements With Maximum Frequency

---
+++ CountElementsWithMaximumFrequency.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+public class CountElementsWithMaximumFrequency {
+
+    public int maxFrequencyElements(int[] nums) {
+        HashMap<Integer, Integer> dp = new HashMap<>();
+        int maxCount = 1;
+        int total = 0;
+
+        for (int num : nums) {
+            if (!dp.containsKey(num)) {
+                dp.put(num, 1);
+
+                if (maxCount == 1) {
+                    total++;
+                }
+            } else {
+                int newCount = dp.get(num) + 1;
+                if (newCount > maxCount) {
+                    maxCount = newCount;
+                    total = newCount;
+                } else if (newCount == maxCount) {
+                    total += newCount;
+                }
+                dp.put(num, newCount);
+            }
+        }
+
+        return total;
+    }
+
+}
