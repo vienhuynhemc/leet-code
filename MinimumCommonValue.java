4b4a4f2da9040838487274cb3ea07a5c1fc7c137
Huỳnh Văn Viên <vien.huynh@mesoneer.io>
2024-03-09 23:46:17 +0700

    Resolve 2540. Minimum Common Value

---
+++ MinimumCommonValue.java
@@ -0,0 +1,31 @@
+public class MinimumCommonValue {
+
+    public int getCommon(int[] nums1, int[] nums2) {
+        int nums1Length = nums1.length;
+        int nums2Length = nums2.length;
+
+        if (nums1[nums1Length - 1] < nums2[0] || nums2[nums2Length - 1] < nums1[0]) {
+            return -1;
+        }
+
+        int rangeArr1 = 0;
+        int rangeArr2 = 0;
+
+        while (rangeArr1 < nums1Length && rangeArr2 < nums2Length) {
+            int num1 = nums1[rangeArr1];
+            int num2 = nums2[rangeArr2];
+            if (num1 == num2) {
+                return num1;
+            }
+
+            if (num1 > num2) {
+                rangeArr2++;
+            } else {
+                rangeArr1++;
+            }
+        }
+
+        return -1;
+    }
+
+}
