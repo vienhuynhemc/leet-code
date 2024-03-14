3eec898085fc0d52396ead03e02593f105ccc15e
Huỳnh Văn Viên <vien.huynh@mesoneer.io>
2024-03-14 23:59:11 +0700

    Resolve 930. Binary Subarrays With Sum

---
+++ BinarySubarraysWithSum.java
@@ -0,0 +1,24 @@
+public class BinarySubarraysWithSum {
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int total = 0;
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            int sum = nums[i];
+            if (sum == goal) {
+                total++;
+            }
+            for (int j = i + 1; j < length; j++) {
+                sum += nums[j];
+                if (sum == goal) {
+                    total++;
+                } else if (sum > goal) {
+                    break;
+                }
+            }
+        }
+
+        return total;
+    }
+
+}
