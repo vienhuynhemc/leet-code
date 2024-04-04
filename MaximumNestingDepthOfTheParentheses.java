16c707b0ef594cde1e7c1885d02707b230708f2a
Huỳnh Văn Viên <vien.huynh@mesoneer.io>
2024-04-04 20:15:19 +0700

    Resolve 1614. Maximum Nesting Depth of the Parentheses

---
+++ MaximumNestingDepthOfTheParentheses.java
@@ -0,0 +1,21 @@
+public class MaximumNestingDepthOfTheParentheses {
+
+    public int maxDepth(String s) {
+        int max = 0;
+        int remain = 0;
+        char[] sChars = s.toCharArray();
+        for (char aChar : sChars) {
+            if (aChar == '(') {
+                remain++;
+            } else if (aChar == ')') {
+                if (remain > max) {
+                    max = remain;
+                }
+                remain--;
+            }
+        }
+
+        return max;
+    }
+
+}
