34429afe9be294eef5a3bb6c5f0ff1b979ea35f3
Huỳnh Văn Viên <vien.huynh@mesoneer.io>
2024-04-05 23:56:43 +0700

    Resolve 1544. Make The String Great

---
+++ MakeTheStringGreat.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class MakeTheStringGreat {
+
+    public String makeGood(String s) {
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (char aChar : chars) {
+            if (stack.isEmpty()) {
+                stack.push(aChar);
+                continue;
+            }
+
+            if (Math.abs(aChar - stack.peek()) == 32) {
+                stack.pop();
+            } else {
+                stack.push(aChar);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        MakeTheStringGreat handler = new MakeTheStringGreat();
+        System.out.println(handler.makeGood("leEeetcode"));
+    }
+
+}
