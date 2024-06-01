0e20f845a309841b0c4df3f27c4e6b41619e20d5
Huỳnh Văn Viên <vien.huynh@mesoneer.io>
2024-06-01 21:38:34 +0700

    Resolve 3110. Score of a String

---
+++ ScoreOfAString.java
@@ -0,0 +1,14 @@
+public class ScoreOfAString {
+
+    public int scoreOfString(String s) {
+        int score = 0;
+        final char[] chars = s.toCharArray();
+
+        for (int i = 0; i < chars.length - 1; i++) {
+            score += Math.abs(chars[i] - chars[i + 1]);
+        }
+
+        return score;
+    }
+
+}
