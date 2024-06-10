58648c998480d7ad4398f0a4f5365631b2f228de
Huỳnh Văn Viên <vien.huynh@mesoneer.io>
2024-06-10 21:03:10 +0700

    Resolve 1051. Height Checker

---
+++ HeightChecker.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class HeightChecker {
+
+    public int heightChecker(int[] heights) {
+        final int[] sorted = heights.clone();
+        Arrays.sort(sorted);
+
+        int count = 0;
+        for (int i = 0; i < sorted.length; i++) {
+            if (sorted[i] != heights[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
