9967c845d18e733eabed7975bf6e02743aa46196
Huỳnh Văn Viên <vien.huynh@mesoneer.io>
2025-01-06 10:10:35 +0700

    Resolve 1732. Find the Highest Altitude

---
+++ FindTheHighestAltitude.java
@@ -0,0 +1,22 @@
+public class FindTheHighestAltitude {
+
+    public int largestAltitude(int[] gain) {
+        int ans = 0;
+        int sum = 0;
+        for (int i = 0; i < gain.length; i++) {
+            sum += gain[i];
+            if (sum > ans) {
+                ans = sum;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        final FindTheHighestAltitude test = new FindTheHighestAltitude();
+        System.out.println(test.largestAltitude(new int[]{-5, 1, 5, 0, -7}));
+        System.out.println(test.largestAltitude(new int[]{-4, -3, -2, -1, 4, 3, 2}));
+    }
+
+}
