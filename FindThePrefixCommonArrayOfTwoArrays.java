6e1e45586c3f1990199897b5d9210814b78bc28b
Huỳnh Văn Viên <vien.huynh@mesoneer.io>
2025-01-14 09:36:48 +0700

    Resolve 2657. Find the Prefix Common Array of Two Arrays

---
+++ FindThePrefixCommonArrayOfTwoArrays.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FindThePrefixCommonArrayOfTwoArrays {
+
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        final Set<Integer> set = new HashSet<>();
+        final int[] ans = new int[A.length];
+        int count = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == B[i]) {
+                count++;
+            } else {
+                if (set.contains(A[i])) {
+                    count++;
+                } else {
+                    set.add(A[i]);
+                }
+
+                if (set.contains(B[i])) {
+                    count++;
+                } else {
+                    set.add(B[i]);
+                }
+            }
+
+            ans[i] = count;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        final FindThePrefixCommonArrayOfTwoArrays test = new FindThePrefixCommonArrayOfTwoArrays();
+        System.out.println(Arrays.toString(test.findThePrefixCommonArray(new int[]{1, 3, 2, 4}, new int[]{3, 1, 2, 4})));
+        System.out.println(Arrays.toString(test.findThePrefixCommonArray(new int[]{2, 3, 1}, new int[]{3, 1, 2})));
+    }
+
+}
