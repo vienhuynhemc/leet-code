063acc21c799df7d59885f75d717ef5140d9a208
Huỳnh Văn Viên <vien.huynh@mesoneer.io>
2025-05-15 15:21:09 +0700

    Resolve 2900. Longest Unequal Adjacent Groups Subsequence I

---
+++ LongestUnequalAdjacentGroupsSubsequenceI.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LongestUnequalAdjacentGroupsSubsequenceI {
+
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+        final List<String> result = new ArrayList<>();
+
+        int currentGroup = groups[0];
+        result.add(words[0]);
+
+        int i = 1;
+        while (i < groups.length) {
+            if (groups[i] == currentGroup) {
+                i++;
+                continue;
+            }
+
+            currentGroup = groups[i];
+            result.add(words[i]);
+            i++;
+        }
+
+        return result;
+    }
+
+}
