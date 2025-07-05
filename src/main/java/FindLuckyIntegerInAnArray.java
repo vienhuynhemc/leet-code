33a971aca61fc8160256f668a7a9c7e8129fda62
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2025-07-05 13:53:27 +0700

    Resolve 1394. Find Lucky Integer in an Array
    Implements a method to find the largest lucky integer in an array, where a lucky integer is defined as one whose value is equal to its frequency in the array.

---
+++ src/main/java/FindLuckyIntegerInAnArray.java
@@ -0,0 +1,26 @@
+package src.main.java;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class FindLuckyIntegerInAnArray {
+
+    public int findLucky(int[] arr) {
+        final Map<Integer, Integer> map = new HashMap<>();
+
+        for (final int number : arr) {
+            map.put(number, map.getOrDefault(number, 0) + 1);
+        }
+
+        int result = -1;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (Objects.equals(entry.getKey(), entry.getValue()) && entry.getKey() > result) {
+                result = entry.getKey();
+            }
+        }
+
+        return result;
+    }
+
+}
