7a924f159c64e596d6ea118fa8bd2e6d3027497f
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2025-07-14 23:27:29 +0700

    Solve 1290. Convert Binary Number in a Linked List to Integer

---
+++ src/main/java/ConvertBinaryNumberInALinkedListToInteger.java
@@ -0,0 +1,31 @@
+package src.main.java;
+
+import src.main.java.model.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertBinaryNumberInALinkedListToInteger {
+
+    public int getDecimalValue(ListNode head) {
+        final List<Integer> data = new ArrayList<>();
+
+        int length = 0;
+        while (head != null) {
+            if (head.val == 1) {
+                data.add(length);
+            }
+
+            length++;
+            head = head.next;
+        }
+
+        int result = 0;
+        for (Integer item : data) {
+            result += (int) Math.pow(2, length - 1 - item);
+        }
+
+        return result;
+    }
+
+}
