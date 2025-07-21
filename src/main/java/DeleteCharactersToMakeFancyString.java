6d9e876e6241cf9df8eeee0de81d574e41ec4f3e
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2025-07-21 21:45:16 +0700

    Solve 1957. Delete Characters to Make Fancy String

---
+++ src/main/java/DeleteCharactersToMakeFancyString.java
@@ -0,0 +1,30 @@
+package src.main.java;
+
+public class DeleteCharactersToMakeFancyString {
+
+    public String makeFancyString(String s) {
+        final StringBuilder builder = new StringBuilder();
+
+        final char[] chars = s.toCharArray();
+
+        int count = 1;
+        char currentChar = chars[0];
+        builder.append(currentChar);
+
+        for (int i = 1; i < chars.length; i++) {
+            if (currentChar == chars[i]) {
+                count++;
+            } else {
+                count = 1;
+                currentChar = chars[i];
+            }
+
+            if (count < 3) {
+                builder.append(currentChar);
+            }
+        }
+
+        return builder.toString();
+    }
+
+}
