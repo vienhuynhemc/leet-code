1610ed3b6d3c07014d03c85a0203f8f996884b14
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2025-09-08 22:03:18 +0700

    Solve 1317. Convert Integer to the Sum of Two No-Zero Integers

---
+++ src/main/java/ConvertIntegerToTheSumOfTwoNoZeroIntegers.java
@@ -0,0 +1,43 @@
+package src.main.java;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConvertIntegerToTheSumOfTwoNoZeroIntegers {
+
+    final Set<Integer> cache = new HashSet<>();
+
+    public int[] getNoZeroIntegers(int n) {
+        final int[] res = new int[2];
+
+        for (int i = 1; i < n; i++) {
+            final boolean isNonZeroInteger = isNonZeroInteger(i);
+            if (!isNonZeroInteger) {
+                continue;
+            }
+
+            cache.add(i);
+
+            final int remain = n - i;
+            if (cache.contains(remain)) {
+                res[0] = i;
+                res[1] = remain;
+                break;
+            }
+        }
+
+        return res;
+    }
+
+    private boolean isNonZeroInteger(int n) {
+        while (n > 9) {
+            if (n % 10 == 0) {
+                return false;
+            }
+            n /= 10;
+        }
+
+        return true;
+    }
+
+}
