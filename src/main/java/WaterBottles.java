93fc0a3dcfa1cb90a046934eb2de3377067bde13
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2025-10-01 20:57:11 +0700

    Solve 1518. Water Bottles

---
+++ src/main/java/WaterBottles.java
@@ -0,0 +1,19 @@
+package src.main.java;
+
+public class WaterBottles {
+
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int result = numBottles;
+
+        int emptyBottles = numBottles;
+        while (emptyBottles >= numExchange) {
+            int newBottles = emptyBottles / numExchange;
+            result += newBottles;
+
+            emptyBottles = emptyBottles % numExchange;
+            emptyBottles += newBottles;
+        }
+
+        return result;
+    }
+}
