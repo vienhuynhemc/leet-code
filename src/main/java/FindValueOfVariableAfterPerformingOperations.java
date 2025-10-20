a1965bbb4cb3e6df938dbf18264bd9cd5023f04c
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2025-10-20 20:56:48 +0700

    Solve 2011. Final Value of Variable After Performing Operations

---
+++ src/main/java/FindValueOfVariableAfterPerformingOperations.java
@@ -0,0 +1,22 @@
+package src.main.java;
+
+import java.util.Map;
+
+public class FindValueOfVariableAfterPerformingOperations {
+
+    private final Map<String, Integer> operationsMap = Map.of(
+            "++X", 1,
+            "X++", 1,
+            "--X", -1,
+            "X--", -1
+    );
+
+    public int finalValueAfterOperations(String[] operations) {
+        int result = 0;
+        for (String operation : operations) {
+            result += operationsMap.get(operation);
+        }
+
+        return result;
+    }
+}
