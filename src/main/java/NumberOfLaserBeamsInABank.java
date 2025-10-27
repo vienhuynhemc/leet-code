146b70b1ee98c25acd903715413faa7b177ed382
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2025-10-27 16:17:37 +0700

    Solve 2125. Number of Laser Beams in a Bank

---
+++ src/main/java/NumberOfLaserBeamsInABank.java
@@ -0,0 +1,31 @@
+package src.main.java;
+
+public class NumberOfLaserBeamsInABank {
+
+    public int numberOfBeams(String[] bank) {
+        int prevDeviceCount = 0;
+        int result = 0;
+
+        for (String s : bank) {
+            final char[] arr = s.toCharArray();
+            int deviceCount = 0;
+            for (char c : arr) {
+                if (c == '1') {
+                    deviceCount++;
+                }
+            }
+
+            if (deviceCount == 0) {
+                continue;
+            }
+
+            if (prevDeviceCount != 0) {
+                result += deviceCount * prevDeviceCount;
+            }
+
+            prevDeviceCount = deviceCount;
+        }
+
+        return result;
+    }
+}
