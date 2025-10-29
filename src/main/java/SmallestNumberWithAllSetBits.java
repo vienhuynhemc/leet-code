6300997df71066ebc9e63e1aeb23d599dba30234
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2025-10-29 19:14:55 +0700

    Solve 3370. Smallest Number With All Set Bits

---
+++ src/main/java/SmallestNumberWithAllSetBits.java
@@ -0,0 +1,16 @@
+package src.main.java;
+
+public class SmallestNumberWithAllSetBits {
+
+    public int smallestNumber(int n) {
+        int result = 1;
+        int temp = 1;
+
+        while (result < n) {
+            temp *= 2;
+            result += temp;
+        }
+
+        return result;
+    }
+}
