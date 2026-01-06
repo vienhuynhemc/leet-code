25794ba532c51a47647f677408c56eae4145b756
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2026-01-06 21:28:20 +0700

    Solve 237. Delete Node in a Linked List

---
+++ src/main/java/DeleteNodeInALinkedList.java
@@ -0,0 +1,10 @@
+/* vienhuynhemc */
+import model.ListNode;
+
+public class DeleteNodeInALinkedList {
+
+  public void deleteNode(ListNode node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+  }
+}
