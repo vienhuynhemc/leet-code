d81af046605756f8be547f48690a587884fdbb1e
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2026-02-13 18:27:57 +0700

    Refactor 2073. Time Needed to Buy Tickets

--- src/main/java/TimeNeededToBuyTickets.java
+++ src/main/java/TimeNeededToBuyTickets.java
@@ -2,17 +2,12 @@
 public class TimeNeededToBuyTickets {
 
   public int timeRequiredToBuy(int[] tickets, int k) {
-    int n = tickets.length;
     int tickedToBy = tickets[k];
 
     int time = 0;
-    for (int i = 0; i < n; i++) {
-      if (i > k) {
-        int ticket = tickets[i];
-        time += ticket >= tickedToBy ? tickedToBy - 1 : ticket;
-      } else {
-        time += Math.min(tickets[i], tickedToBy);
-      }
+    for (int i = 0; i < tickets.length; i++) {
+      if (i > k) time += tickets[i] >= tickedToBy ? tickedToBy - 1 : tickets[i];
+      else time += Math.min(tickets[i], tickedToBy);
     }
 
     return time;
