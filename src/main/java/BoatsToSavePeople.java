e29ea947f3d8765de3803a55f2664c2da61bf88f
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2026-02-19 19:23:05 +0700

    Solve 881. Boats to Save People

---
+++ src/main/java/BoatsToSavePeople.java
@@ -0,0 +1,25 @@
+/* vienhuynhemc */
+import java.util.Arrays;
+
+public class BoatsToSavePeople {
+
+  public int numRescueBoats(int[] people, int limit) {
+    Arrays.sort(people);
+
+    int left = 0;
+    int right = people.length - 1;
+
+    int result = 0;
+
+    while (left <= right) {
+      if (left < right && people[left] + people[right] <= limit) {
+        left++;
+      }
+
+      right--;
+      result++;
+    }
+
+    return result;
+  }
+}
