fd9e53ffc326adc7f1130df8e58389ac719d6574
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2026-02-22 20:14:16 +0700

    Refactor 18. 4Sum

--- src/main/java/FourSum.java
+++ src/main/java/FourSum.java
@@ -6,72 +6,46 @@ import java.util.List;
 public class FourSum {
 
   public List<List<Integer>> fourSum(int[] nums, int target) {
-    if (nums.length < 4) {
-      return new ArrayList<>();
-    }
-
-    List<List<Integer>> quadruplets = new ArrayList<>();
-    int length = nums.length;
-    int endIdxNumOne = length - 3;
-    int endIdxNumTwo = length - 2;
+    if (nums.length < 4) return new ArrayList<>();
 
     Arrays.sort(nums);
+    final List<List<Integer>> result = new ArrayList<>();
 
-    for (int i = 0; i < endIdxNumOne; i++) {
-      if (i != 0 && nums[i] == nums[i - 1]) {
-        continue;
-      }
+    for (int i = 0; i < nums.length - 3; i++) {
+      if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-      int firstNum = nums[i];
+      for (int j = i + 1; j < nums.length - 2; j++) {
+        if (j > i + 1 && nums[j] == nums[j - 1]) continue;
 
-      List<Integer> quadruplet = new ArrayList<>();
-      quadruplet.add(firstNum);
+        final long remain = (long) target - nums[i] - nums[j];
 
-      int firstIdxOfNumTwo = i + 1;
-      for (int j = firstIdxOfNumTwo; j < endIdxNumTwo; j++) {
-        if (j != firstIdxOfNumTwo && nums[j] == nums[j - 1]) {
-          continue;
-        }
+        int leftIndex = j + 1;
+        int rightIndex = nums.length - 1;
 
-        int secondNum = nums[j];
-        long remain = target - ((long) firstNum + secondNum);
+        while (leftIndex < rightIndex) {
+          long sumOfTwoNums = nums[leftIndex] + nums[rightIndex];
 
-        int left = j + 1;
-        int right = length - 1;
+          if (sumOfTwoNums == remain) {
+            final List<Integer> fourSum = new ArrayList<>();
+            fourSum.add(nums[i]);
+            fourSum.add(nums[j]);
+            fourSum.add(nums[leftIndex]);
+            fourSum.add(nums[rightIndex]);
 
-        while (left < right) {
-          long sum = nums[left] + nums[right];
-          if (sum == remain) {
-            // Add mew quadruplet
-            quadruplet.add(secondNum);
-            quadruplet.add(nums[left]);
-            quadruplet.add(nums[right]);
+            result.add(fourSum);
 
-            quadruplets.add(quadruplet);
-
-            // Reset quadruplet to prepare for next quadruplet
-            quadruplet = new ArrayList<>();
-            quadruplet.add(firstNum);
-
-            int leftVal = nums[left];
-
-            left++;
-            while (left < right && nums[left] == leftVal) {
-              left++;
-            }
-          } else if (sum > remain) {
-            right--;
-          } else {
-            left++;
-          }
+            final int thirdNum = nums[leftIndex++];
+            while (leftIndex < rightIndex && nums[leftIndex] == thirdNum) leftIndex++;
+          } else if (sumOfTwoNums > remain) rightIndex--;
+          else leftIndex++;
         }
       }
     }
 
-    return quadruplets;
+    return result;
   }
 
-  public static void main(String[] args) {
+  static void main() {
     FourSum handler = new FourSum();
     System.out.println(handler.fourSum(new int[] { 1000000000, 1000000000, 1000000000, 1000000000 }, -294967296));
   }
