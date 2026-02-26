f5c6dc6662fe32b8b4cc645be2269eed45494113
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2026-02-26 23:06:33 +0700

    Solve 1502. Can Make Arithmetic Progression From Sequence with Java

---
+++ src/main/java/CanMakeArithmeticProgressionFromSequence.java
@@ -0,0 +1,14 @@
+/* vienhuynhemc */
+import java.util.Arrays;
+
+public class CanMakeArithmeticProgressionFromSequence {
+
+  public boolean canMakeArithmeticProgression(int[] arr) {
+    Arrays.sort(arr);
+
+    final int diff = arr[1] - arr[0];
+    for (int i = 2; i < arr.length; i++) if (arr[i] - arr[i - 1] != diff) return false;
+
+    return true;
+  }
+}
