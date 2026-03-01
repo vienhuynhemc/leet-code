d1ed2c8ee5d3a2e8807b64058301ed49283f3880
Vien Huynh <73946516+vienhuynhemc@users.noreply.github.com>
2026-03-01 16:50:38 +0700

    Solve 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers

---
+++ src/main/java/PartitioningIntoMinimumNumberOfDeciBinaryNumbers.java
@@ -0,0 +1,11 @@
+/* vienhuynhemc */
+public class PartitioningIntoMinimumNumberOfDeciBinaryNumbers {
+
+  public int minPartitions(String n) {
+    int result = 0;
+
+    for (int i = 0; i < n.length(); i++) result = Math.max(result, n.charAt(i) - '0');
+
+    return result;
+  }
+}
